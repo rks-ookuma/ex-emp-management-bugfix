76c69ea5967fd6df2d6b04ece261d98fc5464c39
rks-ookuma <takahiro.okuma@rakus.co.jp>
2021-06-08 12:49:13 +0900

    CreateBeanクラスを作成、AdminServiceにpasswordがBcyptEncoderを用いるよう修正

--- src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
+++ src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
@@ -1,6 +1,7 @@
 package jp.co.sample.emp_management.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,12 +21,16 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
 
+	@Autowired
+	private PasswordEncoder encoder;
+
 	/**
 	 * 管理者情報を登録します.
 	 * 
 	 * @param administrator 管理者情報
 	 */
 	public void insert(Administrator administrator) {
+		administrator.setPassword(encoder.encode(administrator.getPassword()));
 		administratorRepository.insert(administrator);
 	}
 
@@ -37,7 +42,8 @@ public class AdministratorService {
 	 * @return 管理者情報 存在しない場合はnullが返ります
 	 */
 	public Administrator login(String mailAddress, String password) {
-		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, password);
+		String hashPassword = encoder.encode("password");
+		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, hashPassword);
 		return administrator;
 	}
 
