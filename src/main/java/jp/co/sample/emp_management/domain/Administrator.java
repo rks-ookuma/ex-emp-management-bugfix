190d396432666d454e4f5b60c7cea3589943078f
rks-ookuma <takahiro.okuma@rakus.co.jp>
2021-06-09 13:23:06 +0900

    Administratorフォームに認証を追加

--- src/main/java/jp/co/sample/emp_management/domain/Administrator.java
+++ src/main/java/jp/co/sample/emp_management/domain/Administrator.java
@@ -1,12 +1,27 @@
 package jp.co.sample.emp_management.domain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 /**
  * 管理者情報を表すドメイン.
  * 
  * @author igamasayuki
  * 
  */
-public class Administrator {
+public class Administrator implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	public enum Authority {
+		ROLE_USER, ROLE_ADMIN
+	};
+
 	/** id(主キー) */
 	private Integer id;
 	/** 名前 */
@@ -15,6 +30,40 @@ public class Administrator {
 	private String mailAddress;
 	/** パスワード */
 	private String password;
+
+	private Authority authority;
+
+	@Override
+	public String getUsername() {
+		return this.mailAddress;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority(authority.toString()));
+		return authorities;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 
 	/**
 	 * 引数無しのコンストラクタ.
@@ -25,14 +74,10 @@ public class Administrator {
 	/**
 	 * 初期化用コンストラクタ.
 	 * 
-	 * @param id
-	 *            id(主キー)
-	 * @param name
-	 *            名前
-	 * @param mailAddress
-	 *            メールアドレス
-	 * @param password
-	 *            パスワード
+	 * @param id          id(主キー)
+	 * @param name        名前
+	 * @param mailAddress メールアドレス
+	 * @param password    パスワード
 	 */
 	public Administrator(Integer id, String name, String mailAddress, String password) {
 		this.id = id;
@@ -97,10 +142,22 @@ public class Administrator {
 		this.password = password;
 	}
 
+	public Authority getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(Authority authority) {
+		this.authority = authority;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	@Override
 	public String toString() {
 		return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
-				+ "]";
+				+ ", authority=" + authority + "]";
 	}
-	
+
 }
